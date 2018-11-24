af4d2db457930c3247a33b46bb7318bcc8b8a56c
Raja Sundaram Ganesan <raja.gan@ascendcorp.com>
2018-11-24 14:42:29 +0700

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+<
+src/index.js,f/9/f900edc44cb929c2267a16100a32851f6d5b477d
+?
+src/App.test.js,9/2/923703ced91ac38943d37930d9d27a7ebe8f01ad
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+=
+src/index.css,c/5/c5d38a62d1faae90a2acaa34b79a7ef365c917e4
+:
+
+src/App.js,1/e/1e43fa63aecd2127be034b3fb9d4caa172275714
+A
+public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import CytoscapeComponent from "react-cytoscapejs";
+import Cytoscape from "cytoscape";
+//import COSEBilkent from "cytoscape-cose-bilkent";
+import dagre from "cytoscape-dagre";
+//Cytoscape.use(dagre);
+//Cytoscape.use(COSEBilkent);
+import styles from './cy-style';
 class App extends Component {
   render() {
+    // const layout = {
+    //   name: "dagre",
+    //   fit: true,
+    //   padding: 30,
+    //   width: function( edge ){ debugger; return edge.weight; }
+    // }; //cose-bilkent, dagre, grid
+
+    const layout = { name: 'cose',
+      idealEdgeLength: 100,
+      nodeOverlap: 20,
+      refresh: 20,
+      fit: true,
+      padding: 30,
+      randomize: false,
+      componentSpacing: 100,
+      nodeRepulsion: 400000,
+      edgeElasticity: 100,
+      nestingFactor: 5,
+      gravity: 80,
+      numIter: 1000,
+      initialTemp: 200,
+      coolingFactor: 0.95,
+      minTemp: 1.0}
+    const dep = [
+      { parent: "bootifulmeters", child: "app_db", callCount: 4 },
+      { parent: "kafka", child: "servicea", callCount: 4 },
+      { parent: "servicea", child: "kafka", callCount: 9 },
+      { parent: "kafka", child: "serviceb", callCount: 3 },
+      { parent: "apip", child: "evcache_g", callCount: 1 },
+      { parent: "apip", child: "api", callCount: 1 },
+      { parent: "api", child: "session_logs", callCount: 1 }
+    ];
+    const services = new Set();
+    const elements = [];
+    dep.forEach(service => {
+      services.add(service.parent);
+      services.add(service.child);
+    });
+
+    services.forEach(service => {
+      let el = { data: { id: service, name: service } };
+      elements.push(el);
+    });
+    dep.forEach(ele => {
+      let el = {
+        data: {
+          source: ele.parent,
+          target: ele.child,
+          weight: ele.callCount,
+          width: ele.callCount
+        }
+      };
+      elements.push(el);
+    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <CytoscapeComponent
+        stylesheet = {styles}
+        // stylesheet={[
+        //   {
+        //     selector: "node",
+        //     style: {
+        //       content: "data(id)",
+        //       shape: "roundrectangle",
+        //       "text-opacity": 0.5,
+        //       "text-valign": "center",
+        //       "text-halign": "right",
+        //       "background-color": "#11479e"
+        //     }
+        //   },
+        //
+        //   {
+        //     selector: "edge",
+        //     style: {
+        //       "curve-style": "bezier",
+        //       "target-arrow-shape": "triangle",
+        //       width: 2,
+        //       "line-color": "#ddd",
+        //       "target-arrow-color": "#ddd"
+        //     }
+        //   }
+        // ]}
+        elements={elements}
+        style={{ minWidth: "800", minHeight: "600px" }}
+        layout={layout}
+      />
     );
   }
 }
